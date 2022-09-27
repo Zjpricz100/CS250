b1c693ed7a6947af8f9365670b8747973e5215e4
Zjpricz100 <zjpricz100@gmail.com>
2022-09-26 23:53:03 -0700

    Started Testing Prep

---
+++ Test1/DoublyList.cpp
@@ -0,0 +1,24 @@
+    #include "DoublyList.h"
+
+using namespace std;
+
+void DoublyList::clearList()
+{ 	
+    Node* temp = first;
+
+    while (first != nullptr)
+    {
+        first = first->getNext();
+        delete temp;
+        temp = first;
+    }
+
+    last = nullptr;		
+    count = 0;
+}
+
+DoublyList::~DoublyList()
+{
+    clearList();
+}
+    
---
+++ Test1/testing2.cpp
@@ -0,0 +1,15 @@
+    #include "DoublyList.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// test cases
+
+	cout << endl ;
+	return 0;
+}
+
+    
